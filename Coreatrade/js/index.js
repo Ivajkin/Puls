787ef26f813dc3d55736e903fdae21edbe06b680
Ivajkin Timofej <timatnet@gmail.com>
2012-08-07 20:40:19 +1100

    Prepared for more.html generation

--- Coreatrade/js/index.js
+++
@@ -1,5 +0,0 @@
-	[   {"mark": "Mark1",	"model": ["1Model1", "1Model2", "1Model3"] },
-		{"mark": "Mark2",	"model": ["2Model1", "2Model2", "2Model3"] },
-		{"mark": "Mark3",	"model": ["3Model1", "3Model2", "3Model3"] },
-		{"mark": "Mark4",	"model": ["4Model1", "4Model2"] }
-	]
---
+++ Coreatrade/module/more.js
@@ -0,0 +1,26 @@
+window.addEvent('domready', function() {
+	dData.forEach(function(car) {
+		$j("tbody#list").append('<tr class="explistrow0&#32;top">' +
+										'<td width="100px">' +
+											'<div class="photo top">' +
+												'<a href="more.html">' +
+													'<span></span>' +
+													'<img src="' + car.image_url + '" />' +
+												'</a>' +
+											'</div>' +
+											'<div class="expimgcount">' +
+																	'4&#32;изобр' +
+											'</div>' +
+										'</td>' +
+										'<td>' +
+											car.name +
+										'</td>' +
+										'<td>' +
+											car.year +
+										'</td>' +
+										'<td>' +
+											car.price + ' р.' +
+										'</td>' +
+									'</tr>');
+	});
+});
