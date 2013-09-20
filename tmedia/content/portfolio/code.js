855595c04cf565864964f8a716f51c97c972d53a
Ayogis <serega.khv@gmail.com>
2013-09-21 09:20:02 +1100

    Amazing Slider for Portfolio

---
+++ tmedia/content/portfolio/code.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: leve_000
+ * Date: 21.09.13
+ * Time: 8:44
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    $('.amazingslider-watermark-0').remove();
+});
--- tmedia/portfolio.php
+++ tmedia/portfolio.php
@@ -2,11 +2,15 @@
 require_once 'namespace.php';
 
     $header_h1 = '<h1 role="heading"><a href="callto:7890"><span class="tcolor">techno</span><span class="mcolor">media</span></a></h1>';
-    $main_block .= 'tpl/article.html';
-    $more_block .= 'tpl/article.html';
-    $script_block .= '';
-    $css_lib = '';
-    $script_lib = '';
+    $main_block .= 'content/portfolio/portfolio.html';
+    $more_block .= '';
+    $script_block .= 'content/portfolio/code.js';
+    $css_lib = '<link href="add/amslider/amslider.css" rel="stylesheet">';
+    $script_lib = <<<EOT
+        <script src="add/amslider/sliderengine/amazingslider.js"></script>
+        <script src="add/amslider/sliderengine/initslider-1.js"></script>
+EOT;
+
 
     include $sitedir.'tpl/tpl.php';
 ?>
